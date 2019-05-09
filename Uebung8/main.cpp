cb0d9049a8be745e9d90394fcc054726f320bc59
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-09 13:10:53 +0200

    First Commit

---
+++ Uebung8/main.cpp
@@ -0,0 +1,32 @@
+
+//Arrayfunktionen
+//
+//Schreiben Sie eine kleine Funktionsbibliothek, die verschiedene Funktionen bereitstellt, um mit Ganzzahlenarrays zu arbeiten.Dabei sollen im Hauptprogramm(main) verschiedene Arrays zu Testzwecken fest programmiert werden(z.B: int myArray[] = { 1,2,3 };
+//int array2[100] = { 0 }; ein Array mit den Werten von 1 - 100 und weitere, eigene Testfälle)
+//
+//Schreiben Sie dann folgende Funktionen, wobei Sie selbst überlegen sollen, welche Parameter übergeben werden müssen und ob es einen Rückgabetyp gibt(return …) Wählen Sie aussagekräftige Namen für Ihre Funktionen und denken Sie an die Übergabe der Elementanzahl.
+//
+//a) Berechnung der Gesamtsumme aller Werte
+//b) Setzen aller Werte auf 0
+//c) Setzen aller Werte auf einen übergebenen Wert
+//c) Suchen, ob eine bestimmte Zahl mindestens einmal vorkommt
+//d) Zählen, wie oft eine bestimmte Zahl im Array vorhanden ist
+//e) Verdoppeln aller Werte im übergebenen Array
+//f) Reihenfolge aller Zahlen vertauschen
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Programm zu Uebung 8" << endl << endl;
+
+
+
+
+	system("pause");
+	return 0;
+}
